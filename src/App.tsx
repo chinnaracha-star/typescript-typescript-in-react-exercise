7da4b5e23f16b08c5e38034ca47d6d3973a656d9
Chindurex <chinnaracha@gmail.com>
2026-07-14 09:37:36 +0700

    refactor: update Greeting component with TypeScript types and render in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import "./App.css";
+import Greeting from "./Greeting";
 
 function App() {
   // TODO4 : Render ออกมาใน App.tsx
-  return;
+  return (
+    <div className="App">
+      <Greeting name="Alice" age={30} />
+    </div>
+  );
 }
 
 export default App;
