09b072eab7a911a571592d8b02f6d43914c79709
Chindurex <chinnaracha@gmail.com>
2026-07-14 10:01:43 +0700

    update routing in App component and define User type in UsersList

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,11 @@ import { useNavigate } from "react-router-dom";
 
 // TODO1: กำหนด Type สำหรับ prop 'name'
 
-const Home = ({ name }) => {
+type HomeProps = {
+  name: string;
+};
+
+const Home = ({ name }: HomeProps) => {
   const navigate = useNavigate();
 
   return (
--- src/pages/UserList.tsx
+++ src/pages/UserList.tsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
 
 // TODO2: กำหนด Type สำหรับ user
+type User = {
+  id: number;
+  name: string;
+};
 
 const UsersList = () => {
-  const users = [
-    { id: 1, name: "Alice" },
+  const users: User[] = [
+    { id: 1, name: "Chin" },
     { id: 2, name: "Bob" },
     { id: 3, name: "Charlie" },
   ];
